b7e7480e97314cc5b151ffeb8aa3fd4b8d50a398
huahuaxx <1840108239@qq.com>
2019-11-22 11:30:54 +0800

    Update run_suite.py

--- run_suite.py
+++ run_suite.py
@@ -16,7 +16,7 @@ suite.addTest(unittest.makeSuite(TestEmp))
 # 定义测试报告的名称
 print("BaseDir的值：", app.BASEDIR)
 # report_name = app.BASEDIR + "/report/ihrm-{}.html".format(time.strftime('%Y%m%d %H%M%S'))
-report_name = app.BASEDIR + "/report/ihrm.html"
+report_name = app.BASEDIR + "/report/ihrm2.html"
 with open(report_name, mode='wb') as f:
     # 定义htmltestrunner的实例
     runner = HTMLTestRunner(f, verbosity=1, title="IHRM人力资源管理系统测试报告", description="登陆模块的测试")
